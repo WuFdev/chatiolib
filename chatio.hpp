d0c4f20bf4099bc6af7ea5409ff00e6951074e89
ladc <72278371+WuFdev@users.noreply.github.com>
2022-07-12 18:43:59 -0400

    Update chatio.hpp

--- chatio.hpp
+++ chatio.hpp
@@ -1,15 +1,37 @@
+#include <iostream>
+using namespace std;
+
 class config {
   public:
-  void port();
+  void room_passw();
+  void login_form();
+  void welcome_msg();
 }
 
 class
 
-void config::port(string port) {
-  system("$./chat_server", port);
+void config::room_passw(string port) {
+  system("$./chat_server ", port);
+}
+
+void config::login_form(bool true = true, bool false = false) 
+{
+  int x;
+  int y;
+  int z;
+  cout << "username: "; // Type a number and press enter
+  cin >> x; // Get user input from the keyboard
+  cout << "ip address:"
+  cout << "room password: ";
+  cin >> z;
+  system("$./chat_client ", x, " ", y, " ", z);
+}
+void config::welcome_msg(string welcome) {
+  cout << welcome;
 }
 
 int init_config() {
   config config;
   #include "config.hpp"
 }
+
